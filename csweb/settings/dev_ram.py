3ed68a61f400478756e1f4353fd024b2d4831842
rmedinahu <rmedina@nmhu.edu>
2018-12-05 16:22:58 -0700

    adjust for django 2.1.4

--- csweb/settings/dev_ram.py
+++ csweb/settings/dev_ram.py
@@ -16,14 +16,16 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-			'NAME': 'cmswebdb',
-			'USER': os.environ['DB_USER'],
-			'PASSWORD': '1',
-			'HOST': 'localhost',
-			'PORT': '5432'
-    }
-}
+# No database required for project.
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+# 			'NAME': 'cmswebdb',
+# 			'USER': os.environ['DB_USER'],
+# 			'PASSWORD': '1',
+# 			'HOST': 'localhost',
+# 			'PORT': '5432'
+#     }
+# }
 
